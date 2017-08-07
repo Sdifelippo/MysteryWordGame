40539b93a31a8e3528369cb3009e9ed6df033fcc
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-06 22:47:51 -0400

    wordgameworkswithsomeknownbugs

--- app.js
+++ app.js
@@ -1,52 +1,125 @@
 const express = require('express');
-const session = require('express-session');
-const mustacheExpress = require('mustache-express');
-const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const fs = require('fs');
-
-
 const app = express();
-const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const mustachExpress = require('mustache-express');
+const fs = require('fs');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const words = fs
+  .readFileSync('/usr/share/dict/words', 'utf-8')
+  .toLowerCase()
+  .split('\n');
 
-app.engine('mustache', mustacheExpress());
-app.set('views', './views');
+
+var randomWord = words[selectRandomWord(0,words.length)];
+if (randomWord.length >= 4 && randomWord <= 6)
+return
+console.log('randomWord', randomWord);
+var randomWordArray = randomWord.split('');
+//variables used globally
+var dashedArray = [];
+var guessedLetter = '';
+var userGuesses = [];
+var errorMessage = '';
+createDashedArray();
+
+function createDashedArray() {
+  for (i = 0; i < randomWordArray.length; i++) {
+    dashedArray.push('_');
+  }
+}
+
+function replaceDashWithLetter(letter) {
+  for (let i = 0; i < randomWord.length; i++) {
+    if (randomWord[i] == letter) {
+      dashedArray[i] = letter;
+    }
+  }
+}
+function selectRandomWord(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//set view engine
+app.engine('mustache', mustachExpress());
+app.set('views', __dirname + '/views');
 app.set('view engine', 'mustache');
 
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(session({
-  secret: '2C44-4D44-WppQ385',
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(expressValidator())
-app.use(express.static('public'));
-
-var word = words[Math.floor(Math.random() * words.length)];
-
-var display = [];
-display = word.split('');
-
-
-
-for (var i = 0; i < display.length; i++) {
-  var dashes = word.split(display[i], "_")
-  // display = dashes;
-console.log(dashes);
-}
-console.log(display);
-
-
+//middleware
+app.use('/', express.static('./public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
+  if (randomWordArray.toString() === dashedArray.toString()) {
+    return res.render('winner', { randomWord: randomWord });
+  }
   res.render('wordGame', {
-    word: word,
-    spaces: display
+    computerWord: dashedArray,
+    guessedLetter: guessedLetter,
+    guessCount: guessCount,
+    userGuess: userGuesses,
+    errorMessage: errorMessage,
+    randomWord: randomWord
   });
 });
 
-app.listen(3000, function() {
-  console.log('GAME ON!!!');
+var guessCount = 8;
+
+app.post('/', function(req, res) {
+  userGuess = req.body;
+  if (userGuesses.indexOf(userGuess.guessedLetter) >= 0) {
+    errorMessage =
+      'You already guessed ' +
+      userGuess.guessedLetter +
+      ' , guess a different letter';
+  } else if (randomWord.indexOf(userGuess.guessedLetter) >= 0) {
+    guessedLetter = userGuess.guessedLetter;
+    for (let i = 0; i < randomWordArray.length; i++) {
+      if (randomWordArray[i] == guessedLetter) {
+        dashedArray[i] = guessedLetter;
+      }
+    }
+    userGuesses.push(guessedLetter);
+    guessCount -= 1;
+  } else if (
+    userGuess.guessedLetter.length > 1 ||
+    userGuess.guessedLetter.length <= 0 ||
+    userGuess.guessedLetter.indexOf(' ') >= 0
+  ) {
+    errorMessage =
+      'You may only guess one letter at a time. No spaces. No Numbers';
+    return res.redirect('/');
+} else if (isNaN(userGuesses)) {guessCount -= 1;
+    userGuesses.push(userGuess.guessedLetter);
+  }
+  if (guessCount <= 0) {
+    guessCount = 8;
+    errorMessage = 'No more Guesses for You.';
+    return res.render('loser', {
+      randomWord: randomWord,
+      errorMessage: errorMessage
+    });
+  }
+
+  res.redirect('/');
+});
+
+app.post('/again', function(req, res) {
+  randomWord = words[selectRandomWord(0, words.length)];
+    console.log("random word", randomWord);  randomWordArray = randomWord.split('');
+  dashedArray = [];
+  guessedLetter = '';
+  userGuesses = [];
+  errorMessage = '';
+  guessCount = 8;
+  createDashedArray();
+  res.redirect('/');
+});
+
+app.post('/error', function(req, res){
+  errorMessage = '';
+  res.redirect('/');
+})
+
+app.listen(3000, function (){
+  console.log('Game is on Human...Let\'s GO!!');
 });
